42580488684a1b702e063377e6facf9d8bcf6656
Usama <ser.musama@gmail.com>
2024-12-13 12:50:25 +0500

    abc

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
 
 import "./App.css";
-import HomePage from "./pages/HomePage";
-import CreateTodo from "./pages/CreateTodo";
-import EditTodo from "./pages/EditTodo";
-import Root from "./Root";
-import ErrorPage from "./pages/ErrorPage";
+
 import router from "./Routes";
 
 function App() {
